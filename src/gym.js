6897be41b40c50dbe67e73c41257c094a5c90746
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-04-16 12:44:15 -0400

    removed debuggers

--- src/gym.js
+++ src/gym.js
@@ -1,18 +1,14 @@
-//console.log("in gym.js")
 
 class Gym {
     constructor(gym, gymAttributes){
         this.id = gym.id
-        //debugger
         this.name = gymAttributes.name
         this.description = gymAttributes.description
         this.location = gymAttributes.location
         Gym.all.push(this);
-        //debugger
     }
 
     renderGymCard() {
-        //debugger
         return `
         <div data-id=${this.id}>
           <h3>${this.name} - id: ${this.id}</h3>
