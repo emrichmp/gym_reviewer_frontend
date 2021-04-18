53ae73ef2cd03d6ec507fbdd50a7f512c4bd02dd
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-04-18 19:55:07 -0400

    started form handler in gym for review, started writing callback to this function in App.js

--- src/gym.js
+++ src/gym.js
@@ -43,6 +43,15 @@ class Gym {
         </form>
       `;
       }
+
+      createReviewFormHandler(e) {
+        e.preventDefault()
+        const contentInput = document.querySelector('#input-content').value
+        const ratingInput = document.querySelector('#input-rating').value
+        console.log(contentInput)
+        console.log(ratingInput)
+        //postFetchReviews(contentInput, ratingInput)
+    }
 }
 
 Gym.all = [];
