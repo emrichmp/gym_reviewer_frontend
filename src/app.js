0737d1d8957e9bd3e2dd0133d6a8b8943f2683f0
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-04-18 20:09:37 -0400

    got rid of += renderReviewForm() so user cant spam add the form

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ class App {
         const gym = Gym.findById(id)
         //console.log(id)
         console.log(gym)
-        document.querySelector('#review-form').innerHTML += gym.renderReviewForm()
+        document.querySelector('#review-form').innerHTML = gym.renderReviewForm()
       });
     //   document.querySelector('#review-form').addEventListener('submit', (e) => 
     //   gym.createReviewFormHandler(e))
